37d4fa6daa9760a6fd773f6a40d89053a066cccf
saraketcham <sara.ketcham@gmail.com>
2024-02-28 08:27:56 -0800

    better default.py starting page

--- app/routes/default.py
+++ app/routes/default.py
@@ -2,10 +2,14 @@ from app import app
 from flask import render_template
 
 # This is for rendering the home page
-@app.route('/') #ask why this isnt working
+@app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/aboutus')
-def aboutus():
-    return render_template('aboutus.html')
+# Here's the basics for making any new route
+# It's a good idea to use the same name/word, and same capitalization conventions
+# Add a route name, function name, and html template name 
+# (then go make that html template)
+@app.route('/newnamehere')
+def newnamehere():
+    return render_template('newnamehere.html')
